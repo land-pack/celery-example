bfc18497f70d0e59c77587f4e098a57c52033b75
landpack <ada.frank.ak@gmail.com>
2017-03-08 23:31:24 +0800

    [add] json config

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,17 @@
+import sys
+import json
 
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+print 'args-->',sys.argv
+cere_param = [i for i in sys.argv if i.startswith('--config')][0]
+value_param = cere_param.split('=')[1]
+print 'value-->',value_param
+
+db_url = ''
+with open(value_param,'r') as fd:
+    global db_url
+    conf = json.load(fd)
+    db_url = conf.get('redis_db',{}).get('db_foo')
+print 'db_url-->',db_url
+
+BROKER_URL = db_url
+CELERY_RESULT_BACKEND = db_url
